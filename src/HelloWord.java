495c9435060f1a2494ff6e741c4a760b74615b67
liukai <liukai30@meituan.com>
2018-07-04 22:29:27 +0800

    update

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -3,6 +3,9 @@
  */
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+        System.out.println("hello world3");
+        System.out.println("hello world4");
     }
 }
